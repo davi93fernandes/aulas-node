67a3757c969f501654f22b2a8c42aeabad478514
davi93fernandes <davi.a.fernandes@hotmail.com>
2024-08-05 09:09:47 -0300

    feat: Utiliza yargs como leitor de argumentos.

---
+++ aula01/exemplo2.js
@@ -0,0 +1,10 @@
+
+const yargs = require('yargs/yargs');
+
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv; 
+
+console.log(argv.nome);
+console.log(argv.idade);
+console.log(argv.cidade);
+
--- exemplo1.js
+++
@@ -1,5 +0,0 @@
-const args = process.argv;
-
-console.log(args[2]);
-
-console.log(`Aula de Node 1 - argumento: ${args[2]}`);
